c3b5cfbe1c9802fe77f8eeda55a0ba357a121d80
GlitchyDragon7 <154821369+GlitchyDragon7@users.noreply.github.com>
2024-03-11 08:01:35 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+parent_id = "15euQF4snfR7nwB5DbOgX6dZ4UEFvyT1B"
+db_uri = "mongodb+srv://fabil:kochu12345@sa-resources.6uhvi3w.mongodb.net/?retryWrites=true&w=majority&appName=SA-RESOURCES"
+username_for_logging_into_sa_drive = 'admin'
+password_for_logging_into_sa_drive = 'admin'
